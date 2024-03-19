fc090b6664aae1746c7286d55cde24327d1f458a
stian.larsen <stian.larsen@sparebank1.no>
2024-03-19 07:16:39 +0100

    Updated sitemaps

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,40 +2,27 @@ import { BASE_URL } from "@/lib/consts";
 import { MetadataRoute } from "next";
 import { getAllProjects } from "./projects/api";
 
-// export default function sitemap(): MetadataRoute.Sitemap {
-//   return [
-//     {
-//       url: BASE_URL,
-//       lastModified: new Date(),
-//       changeFrequency: "yearly",
-//       priority: 1,
-//     },
-//     {
-//       url: `${BASE_URL}/projects`,
-//       lastModified: new Date(),
-//       changeFrequency: "yearly",
-//       priority: 0.8,
-//     },
-//   ];
-// }
-
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const projects = await getAllProjects();
-  const projectsMap = projects.map(
+  const dynamicRoutes: MetadataRoute.Sitemap = projects.map(
     ({ currentSlug: projectSlugID, publishedDate }) => ({
       url: `${BASE_URL}/projects/${projectSlugID}`,
       lastModified: publishedDate
         ? new Date(publishedDate).toISOString()
         : new Date().toISOString(),
+      priority: 0.9,
+      changeFrequency: "monthly",
     }),
   );
 
-  const routes = ["", "/projects", "/cv"].map((route) => ({
-    url: `${BASE_URL}${route}`,
-    lastModified: new Date().toISOString(),
-    priority: 1,
-    changeFrequency: "yearly",
-  }));
+  const staticRoutes: MetadataRoute.Sitemap = ["", "/projects", "/cv"].map(
+    (route) => ({
+      url: `${BASE_URL}${route}`,
+      lastModified: new Date().toISOString(),
+      priority: 1,
+      changeFrequency: "monthly",
+    }),
+  );
 
-  return [...routes, ...projectsMap];
+  return [...staticRoutes, ...dynamicRoutes];
 }
